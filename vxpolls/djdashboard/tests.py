12e20164fd153c8676c565f2fb723379050ce7c2
Simon de Haan <simon@praekeltfoundation.org>
2012-03-26 12:45:23 +0200

    moving django based dashboard from afrop over to vxpolls

---
+++ vxpolls/djdashboard/tests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+
+class ApiTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_active(self):
+        pass
+
+    def test_results(self):
+        pass
+
+    def test_completed(self):
+        pass
+
+    def test_export_results(self):
+        pass
+
+    def test_export_users(self):
+        pass

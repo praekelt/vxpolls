2944e5a9168a2000a7b6f945b86090bf2025b22c
Simon de Haan <simon@praekeltfoundation.org>
2012-03-26 12:52:20 +0200

    fixing module name typo

--- vxpolls/djdashboard/urls.py
+++ vxpolls/djdashboard/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from afropinions.webapp.dashboard import views
+from vxpolls.djdashboard import views
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),

74bc680f0f02ebf15675b2d71e3b184b7ae8f374
Simon de Haan <simon@praekeltfoundation.org>
2012-03-28 16:09:40 +0200

    making vxpolls a full Django project to enable editing via Django forms

--- vxpolls/__init__.py
+++
@@ -1,5 +0,0 @@
-from vxpolls.manager import PollManager
-from vxpolls.results import ResultManager
-from vxpolls.dashboard import PollDashboardServer
-
-__all__ = ['PollDashboardServer', 'ResultManager', 'PollManager']

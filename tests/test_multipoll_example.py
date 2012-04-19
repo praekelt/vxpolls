347fc1394c9682f51b732c21b6722011f8a46bf9
dmaclay <dmaclay@gmail.com>
2012-04-19 13:04:10 +0200

    multi poll reg

--- tests/test_multipoll_example.py
+++ tests/test_multipoll_example.py
@@ -10,7 +10,7 @@ class MultiPollApplicationTestCase(ApplicationTestCase):
     application_class = MultiPollApplication
 
     timeout = 1
-    poll_ids = ['register', 'week1', 'week2', 'week3']
+    poll_id_list = ['register', 'week1', 'week2', 'week3']
     default_questions_dict = {
             'register': [{
                 'copy': 'What is your name?',
@@ -29,5 +29,17 @@ class MultiPollApplicationTestCase(ApplicationTestCase):
             'week3': [],
             }
 
+    @inlineCallbacks
+    def setUp(self):
+        yield super(MultiPollApplicationTestCase, self).setUp()
+        self.config = {
+            'poll_id_list': self.poll_id_list,
+            'questions_dict': self.default_questions_dict,
+            'transport_name': self.transport_name,
+            'batch_size': 2,
+        }
+        self.app = yield self.get_application(self.config)
+
     def test_pass(self):
+        #print self.__dict__
         pass
--- vxpolls/multipoll_example.py
+++ vxpolls/multipoll_example.py
@@ -22,3 +22,23 @@ class MultiPollApplication(PollApplication):
                                 'complete the rest.'
     survey_completed_response = 'You have completed this weeks questions '\
                                 'please dial in again next week for more.'
+
+    def validate_config(self):
+        self.questions_dict = self.config.get('questions_dict', {})
+        self.r_config = self.config.get('redis_config', {})
+        self.batch_size = self.config.get('batch_size', 5)
+        self.dashboard_port = int(self.config.get('dashboard_port', 8000))
+        self.dashboard_prefix = self.config.get('dashboard_path_prefix', '/')
+        self.poll_id_list = self.config.get('poll_id_list',
+                                            [self.generate_unique_id()])
+
+    def setup_application(self):
+        self.r_server = FakeRedis(**self.r_config)
+        self.pm = PollManager(self.r_server)
+        if not self.pm.exists(self.poll_id_list):
+            for p in self.poll_id_list:
+                #print p, self.questions_dict.get(p, [])
+                self.pm.register(p, {
+                    'questions': self.questions_dict.get(p, []),
+                    'batch_size': self.batch_size,
+                    })

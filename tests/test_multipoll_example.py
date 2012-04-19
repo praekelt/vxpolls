814f98f6634cce4eac3c6863f182e0114dbe67cc
dmaclay <dmaclay@gmail.com>
2012-04-19 12:41:52 +0200

    list of poll ids

--- tests/test_multipoll_example.py
+++ tests/test_multipoll_example.py
@@ -4,9 +4,30 @@ from vumi.application.tests.test_base import ApplicationTestCase
 
 from vxpolls.multipoll_example import MultiPollApplication
 
+
 class MultiPollApplicationTestCase(ApplicationTestCase):
 
     application_class = MultiPollApplication
+
+    timeout = 1
+    poll_ids = ['register', 'week1', 'week2', 'week3']
+    default_questions_dict = {
+            'register': [{
+                'copy': 'What is your name?',
+                'valid_responses': [],
+                },
+                {
+                'copy': 'Orange, Yellow or Black?',
+                'valid_responses': ['orange', 'yellow', 'black'],
+                },
+                {
+                'copy': 'What is your favorite fruit?',
+                'valid_responses': ['apple', 'orange'],
+                }],
+            'week1': [],
+            'week2': [],
+            'week3': [],
+            }
 
     def test_pass(self):
         pass

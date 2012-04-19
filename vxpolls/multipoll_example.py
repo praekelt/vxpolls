8b9b3a8b6a8903b7b8c8e9c31d9e4493cc7e4f5d
dmaclay <dmaclay@gmail.com>
2012-04-19 12:47:31 +0200

    registration messages

--- vxpolls/multipoll_example.py
+++ vxpolls/multipoll_example.py
@@ -11,12 +11,14 @@ from vxpolls import PollManager
 
 class MultiPollApplication(PollApplication):
 
-    registertration_partial_response = ''
-
-    registertration_completed_response = ''
+    registration_partial_response = 'You have done part of the registration '\
+                                    'process, dail in again to complete '\
+                                    'your registration.'
+    registration_completed_response = 'You have completed registration, '\
+                                      'dial in again to start the surveys.'
 
     batch_completed_response = 'You have completed the first batch of '\
-                                'this weeks questions, dial in again to complete '\
-                                'the rest.'
+                                'this weeks questions, dial in again to '\
+                                'complete the rest.'
     survey_completed_response = 'You have completed this weeks questions '\
                                 'please dial in again next week for more.'

0f8bbda35f2f4870c9b0801308cca690ec46017d
Simon de Haan <simon@praekeltfoundation.org>
2012-08-20 22:50:00 +0200

    reintroducing the sync RedisManager

--- vxpolls/djdashboard/views.py
+++ vxpolls/djdashboard/views.py
@@ -1,4 +1,3 @@
-import redis
 import json
 from django.shortcuts import render, Http404
 from django.http import HttpResponse
@@ -6,7 +5,10 @@ from django.conf import settings
 
 from vxpolls.manager import PollManager
 
-redis = redis.Redis(**settings.VXPOLLS_REDIS_CONFIG)
+from vumi.persist.redis_manager import RedisManager
+
+
+redis = RedisManager.from_config(settings.VXPOLLS_REDIS_CONFIG)
 
 poll_manager = PollManager(redis, settings.VXPOLLS_PREFIX)
 

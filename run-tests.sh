eb7875e8cd123d97d24bf92c8b47703aa399b2e1
Simon de Haan <simon@praekeltfoundation.org>
2012-08-23 16:10:00 +0200

    jenkins test setup

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,18 @@
-virtualenv --no-site-packages ve && \
-source ve/bin/activate && \
-pip install -r requirements.pip && \
-DJANGO_SETTINGS_MODULE=vxpolls.settings PYTHONPATH=. trial tests && \
-DJANGO_SETTINGS_MODULE=vxpolls.settings PYTHONPATH=. django-admin.py test djdashboard content
+#!/bin/sh -e
+
+export DJANGO_SETTINGS_MODULE=vxpolls.settings
+export PYTHONPATH=.
+
+echo "=== Nuking old .pyc files..."
+find vxpolls/ -name '*.pyc' -delete
+echo "=== Erasing previous coverage data..."
+coverage erase
+echo "=== Running trial tests..."
+coverage run --include='vxpolls/*' `which trial` --reporter=subunit tests | tee results.txt | subunit2pyunit
+subunit2junitxml <results.txt >test_results.xml
+rm results.txt
+echo "=== Processing coverage data..."
+coverage xml
+echo "=== Checking for PEP-8 violations..."
+pep8 --repeat vxpolls | tee pep8.txt
+echo "=== Done."

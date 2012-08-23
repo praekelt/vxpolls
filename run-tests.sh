433ec161062e14403c3c97838ade43e9c1ff3efa
Simon de Haan <simon@praekeltfoundation.org>
2012-08-23 15:24:55 +0200

    having ./run-tests create venv

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,5 @@
+virtualenv --no-site-packages ve && \
+source ve/bin/activate && \
+pip install -r requirements.pip && \
 DJANGO_SETTINGS_MODULE=vxpolls.settings PYTHONPATH=. trial tests && \
 DJANGO_SETTINGS_MODULE=vxpolls.settings PYTHONPATH=. django-admin.py test djdashboard content

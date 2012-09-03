4cb90b4dae7ca375731d74651d1515820673602c
dmaclay <dmaclay@gmail.com>
2012-09-03 13:45:10 +0200

    test for session key prefixes

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -40,6 +40,12 @@ class PollManagerTestCase(TestCase):
     def tearDown(self):
         yield self.poll_manager.stop()
 
+    def test_session_key_prefixes(self):
+        expected_redis_key_prefix = "%s:session" % (self.poll_manager.r_prefix)
+        actual_redis_key_prefix = \
+                        self.poll_manager.session_manager.redis._key_prefix
+        self.assertEqual(actual_redis_key_prefix, expected_redis_key_prefix)
+
     @inlineCallbacks
     def test_invalid_input_response(self):
         expected_question = 'What is your favorite colour?'

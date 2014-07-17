632293ddb33384ea45cdb68ae07439751e7a62a0
Jeremy Thurgood <firxen@gmail.com>
2014-07-17 15:03:08 +0200

    Update README and setup.py for release.

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "vxpolls",
-    version = "0.1",
-    url = 'http://github.com/praekelt/vxpolls',
-    license = 'BSD',
-    description = "Simple polling / survey framework for Vumi apps",
-    author = 'Praekelt Foundation',
-    packages = find_packages(),
-    install_requires = [],
+    name="vxpolls",
+    version="0.1.0",
+    url='http://github.com/praekelt/vxpolls',
+    license='BSD',
+    description="Simple polling / survey framework for Vumi apps",
+    long_description=open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekeltfoundation.org',
+    packages=find_packages(),
+    install_requires=[
+        'vumi',
+    ],
+    classifiers=[
+        'Development Status :: 7 - Inactive',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
-
